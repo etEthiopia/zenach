0edd7ca8606aaeb5e4330147154004121b79a927
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-09-08 22:08:57 +0300

    data fetched from a static list

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+export default {
+    products: [{
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'shirts',
+            price: 40,
+            brand: 'Nike',
+            rating: 4.5,
+            reveiwNo: 10
+        },
+        {
+            name: 'Fit Shirt',
+            image: '/images/d1.jpg',
+            category: 'shirts',
+            price: 70,
+            brand: 'Addidas',
+            rating: 4.7,
+            reveiwNo: 8
+        },
+        {
+            name: 'Classic Shirt',
+            image: '/images/d1.jpg',
+            category: 'shirts',
+            price: 40,
+            brand: 'Nike',
+            rating: 4.5,
+            reveiwNo: 10
+        },
+        {
+            name: 'Sweat Pant',
+            image: '/images/d1.jpg',
+            category: 'pants',
+            price: 45,
+            brand: 'Puma',
+            rating: 4.0,
+            reveiwNo: 10
+        },
+        {
+            name: 'Jeans',
+            image: '/images/d1.jpg',
+            category: 'pants',
+            price: 65,
+            brand: 'Zara',
+            rating: 4.0,
+            reveiwNo: 10
+        },
+        {
+            name: 'Skinny Jeans',
+            image: '/images/d1.jpg',
+            category: 'pants',
+            price: 65,
+            brand: 'Zara',
+            rating: 4.0,
+            reveiwNo: 10
+        },
+    ]
+}
