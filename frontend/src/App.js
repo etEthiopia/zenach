3f6e5aa905c5b4cb2e1814b7c35081e2bb3ede5e
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-09-09 01:25:55 +0300

    next to the node js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import data from './data';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
+	const openMenu = () => {
+		document.querySelector('.sidebar').classList.add('open');
+	};
 
-  const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
-  }
+	const closeMenu = () => {
+		document.querySelector('.sidebar').classList.remove('open');
+	};
 
-  const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open")
-  }
- 
-  
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="header">
+					<div className="brand">
+						<button onClick={openMenu}>&#9776;</button>
+						<Link to="/">Zenaጭ</Link>
+					</div>
+					<div className="header-links">
+						<a href="cart.html">Cart</a>
+						<a href="signin.html">Sign In</a>
+					</div>
+				</header>
+				<aside className="sidebar">
+					<h3>Shopping Categories</h3>
+					<button className="sidebar-close-button" onClick={closeMenu}>
+						x
+					</button>
+					<ul>
+						<li>
+							<a href="index.html">Pants</a>
+						</li>
 
-  return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Zenaጭ</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-				</button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            {
-              data.products.map(product => <li>
-                <div className="product">
-                  <img className="product-image" src={product.image} alt="product" />
-                  <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">${product.price}</div>
-                  <div className="product-rating">{product.rating} Stars ({product.reveiwNo} Reviews)</div>
-                </div>
-              </li>)
-            }
-            
-            
-          </ul>
-        </div>
-      </main>
-      <footer className="footer">All right reserved.</footer>
-    </div>
-  );
+						<li>
+							<a href="index.html">Shirts</a>
+						</li>
+					</ul>
+				</aside>
+				<main className="main">
+					<div className="content">
+						<Route path="/product/:id" component={ProductScreen} />
+						<Route path="/" exact={true} component={HomeScreen} />
+					</div>
+				</main>
+				<footer className="footer">All right reserved.</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
-
 
 export default App;
