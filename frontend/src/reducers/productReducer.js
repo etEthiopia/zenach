057b855293dbc13c358876d50e156234ae192cd7
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-09-10 21:38:59 +0300

    much better redundancy

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -28,24 +28,9 @@ function productDetailsReducer(state = { product: {}, images: [] }, action) {
 		case PRODUCT_DETAILS_REQUEST:
 			return { loading: true };
 		case PRODUCT_DETAILS_SUCCESS:
-			var prod = {
-				id: null,
-				name: null,
-				category: null,
-				price: null,
-				brand: null,
-				rating: null,
-				reveiwNo: null
-			};
-			prod.id = action.payload._id;
-			prod.name = action.payload.name;
-			prod.category = action.payload.category;
-			prod.price = action.payload.price;
-			prod.brand = action.payload.brand;
-			prod.rating = action.payload.rating;
-			prod.reveiwNo = action.payload.reveiwNo;
-			
-			return { loading: false, product: prod, images: action.payload.images };
+			const imagegallery = action.payload.images;
+			action.payload.images = null;
+			return { loading: false, product: action.payload, images: imagegallery };
 
 		case PRODUCT_DETAILS_FAIL:
 			return { loading: false, error: action.payload };
