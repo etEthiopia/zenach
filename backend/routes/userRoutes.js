10d06e328359242e95cdf351b24c35fd1ce8ded2
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-09-11 17:52:14 +0300

    signin, createadmin & signup : jwt and bcrypt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,125 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
+require('dotenv').config();
+
+// @Route POST api/users/signin
+// @desc POST Sign In a User
+// @access Public
+router.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+
+	// Simple Validation
+	if (!email || !password) {
+		res.status(400).json({
+			success: false,
+			message: 'All fields are not available.'
+		});
+	}
+
+	await User.findOne({
+		email
+	})
+		.then((user) => {
+			if (!user) {
+				res.status(400).json({
+					success: false,
+					message: 'user does not exist.'
+				});
+			} else {
+				// Validate Password
+				bcrypt
+					.compare(password, user.password)
+					.then((isMatch) => {
+						if (!isMatch) {
+							res.status(400).json({
+								success: false,
+								message: 'Invalid Credientials.'
+							});
+						} else {
+							jwt.sign(
+								{
+									id: user.id
+								},
+								process.env.JWTSECRET,
+								{
+									expiresIn: '1 day'
+								},
+								(err, token) => {
+									if (err) {
+										throw err;
+									}
+									res.json({
+										token: token,
+										user: {
+											id: user.id,
+											name: user.name,
+											email: user.email,
+											type: user.type
+										},
+										success: true
+									});
+								}
+							);
+						}
+					})
+					.catch((err) =>
+						res.json({
+							message: err.toString(),
+							success: false
+						})
+					);
+			}
+		})
+		.catch((err) =>
+			res.json({
+				message: err,
+				success: false
+			})
+		);
+});
+
+// @Route POST api/users/signup
+// @desc POST Add a User
+// @access Public
+router.post('/signup', async (req, res) => {
+	const { name, email, password } = req.body;
+	const user = new User({
+		name: name,
+		email: email,
+		password: password
+	});
+
+	bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) throw err;
+			user.password = hash;
+			user
+				.save()
+				.then((user) => {
+					res.status(201).json(user)['success'] = true;
+				})
+				.catch((err) => {
+					if (res.statusCode == 200) {
+						const message = err.toString();
+						const val = message.search('duplicate key');
+						if (val != -1) {
+							res.status(409).json({
+								message: 'There is already an account by the entered email',
+								success: false
+							});
+						}
+					} else {
+						res.json({
+							message: err.toString(),
+							success: false
+						});
+					}
+				});
+		});
+	});
+});
 
 // @Route POST api/users/createadmin
 // @desc POST Add an Admin
@@ -11,28 +131,34 @@ router.post('/createadmin', async (req, res) => {
 		password: 'daginegussu',
 		type: 'admin'
 	});
-	await admin
-		.save()
-		.then((admin) => {
-			res.status(201).json(admin)['success'] = true;
-		})
-		.catch((err) => {
-			if (res.statusCode == 200) {
-				const message = err.toString();
-				const val = message.search('duplicate key');
-				if (val != -1) {
-					res.status(409).json({
-						message: 'There is already an account by the entered email',
-						success: false
-					});
-				}
-			} else {
-				res.json({
-					message: err.toString(),
-					success: false
+	bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(admin.password, salt, (err, hash) => {
+			if (err) throw err;
+			admin.password = hash;
+			admin
+				.save()
+				.then((admin) => {
+					res.status(201).json(admin)['success'] = true;
+				})
+				.catch((err) => {
+					if (res.statusCode == 200) {
+						const message = err.toString();
+						const val = message.search('duplicate key');
+						if (val != -1) {
+							res.status(409).json({
+								message: 'There is already an account by the entered email',
+								success: false
+							});
+						}
+					} else {
+						res.json({
+							message: err.toString(),
+							success: false
+						});
+					}
 				});
-			}
 		});
+	});
 });
 
 module.exports = router;
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 zenach shopping website
+
+MONGODB_URL= 'mongodb+srv://user:password@cluster0.jkw5c.mongodb.net/zenachdb?retryWrites=true&w=majority'
